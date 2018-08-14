3bc86f16e4b22da5f70ef05454700a8ef524cc1c
Matthew Clark <matthew.clark.dev@gmail.com>
2018-08-13 19:51:58 -0500

    Added runner for Stacks.

---
+++ src/prj/clark/cs/dsa/struct/stack/StackRunner.java
@@ -0,0 +1,72 @@
+package prj.clark.cs.dsa.struct.stack;
+
+import java.util.function.Consumer;
+
+public class StackRunner {
+    private static final int PROGRAM_WIDTH = 30;
+
+    private static final Consumer<Stack> push = stack -> stack.push("");
+    private static final Consumer<Stack> pop = Stack::pop;
+
+    public static void main(String[] args) {
+        long start, end;
+
+        System.out.printf("%8s | %8s | %8s | %12s\n",
+                "Impl", "Method", "Count", "Time");
+
+        int iterations = getIterations(args);
+
+        Stack arrayStack = new ArrayStack();
+        Stack linkedListStack = new LinkedListStack();
+
+        long[] arrayStackResult = timeStackOperation(
+                arrayStack, push, iterations);
+
+        System.out.println(resultOutput(
+                "Array", "Push", iterations, elapsed(arrayStackResult)));
+
+        long[] listStackResult = timeStackOperation(
+                linkedListStack, push, iterations);
+
+        System.out.println(resultOutput(
+                "Linked", "Push", iterations, elapsed(listStackResult)));
+
+        arrayStackResult = timeStackOperation(
+                arrayStack, pop, iterations);
+
+        System.out.println(resultOutput(
+                "Array", "Pop", iterations, elapsed(arrayStackResult)));
+
+        listStackResult = timeStackOperation(
+                linkedListStack, pop, iterations);
+
+        System.out.println(resultOutput(
+                "Linked", "Pop", iterations, elapsed(listStackResult)));
+    }
+
+    private static String resultOutput(String implementation, String method, int iterations, long time) {
+        return String.format("%8s | %8s | %8d | %12d",
+                implementation, method, iterations, time);
+    }
+
+    private static long[] timeStackOperation(Stack stack, Consumer<Stack> fn, int cycles) {
+        long start, stop;
+        start = System.currentTimeMillis();
+
+        for (int i = 0; i < cycles; ++i) {
+            fn.accept(stack);
+        }
+
+        stop = System.currentTimeMillis();
+
+        return new long[]{start, stop};
+    }
+
+    private static int getIterations(String[] args) {
+        return 10000000;
+    }
+
+    private static long elapsed(long[] longs) {
+        return longs[1] - longs[0];
+    }
+}
