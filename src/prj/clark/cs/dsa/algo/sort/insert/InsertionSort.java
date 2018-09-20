c07a3a2129a9173c48a50fd9a4432d679183507b
Matthew Clark <matthew.clark.dev@gmail.com>
2018-09-20 12:11:56 -0500

    Remove extraneous while loop from insertion sort code.

--- src/prj/clark/cs/dsa/algo/sort/insert/InsertionSort.java
+++ src/prj/clark/cs/dsa/algo/sort/insert/InsertionSort.java
@@ -13,17 +13,15 @@ public class InsertionSort implements Sorter {
     @Override
     public <T> void sort(T[] objs, Comparator<T> cmp) {
         for (int i = 0; i < objs.length; ++i) {
+            // Move from the current position back through the array.
             for (int j = i; j > 0; --j) {
+                // If the previous element is smaller, then swap it.
                 if (cmp.compare(objs[j], objs[j - 1]) < 0) {
                     exch(objs, j, j - 1);
                 } else {
+                    // Otherwise, the array up to that point should be in order, and we can stop.
                     break;
                 }
-            }
-
-            int k = i;
-            while (k > 0 && cmp.compare(objs[k], objs[k - 1]) < 0) {
-                exch(objs, k , --k);
             }
         }
     }
