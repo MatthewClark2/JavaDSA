9c98a57110b26604cece3e485c96300c72b7cbba
Matthew Clark <matthew.clark.dev@gmail.com>
2018-11-08 21:54:07 -0600

    Update unit tests after realizing how Kruskal's algorithm actually constructs the MST.

--- test/prj/clark/cs/dsa/algo/mst/KruskalMinimumSpanningTreeTest.java
+++ test/prj/clark/cs/dsa/algo/mst/KruskalMinimumSpanningTreeTest.java
@@ -69,7 +69,7 @@ public class KruskalMinimumSpanningTreeTest {
     @Test
     public void basicGraphMSTWeight() {
         MinimumSpanningTree mst = new KruskalMinimumSpanningTree(graphB);
-        assertEquals(15, mst.weight(), 0);
+        assertEquals(11, mst.weight(), 0);
     }
 
     @Test
@@ -79,10 +79,10 @@ public class KruskalMinimumSpanningTreeTest {
         Set<Edge> expected = new HashSet<>();
 
         expected.add(edge(0, 1, 2));
-        expected.add(edge(1, 3, 1));
-        expected.add(edge(3, 5, 5));
         expected.add(edge(1, 2, 2));
+        expected.add(edge(1, 3, 1));
         expected.add(edge(2, 4, 4));
+        expected.add(edge(4, 5, 2));
 
         for (Edge e : mst.edges()) {
             assertTrue(expected.contains(e));
